7cd441db1a5e085cc04fb71706a13ac524ff15d4
kmira <mira@coyobi.com>
2021-08-07 00:39:20 +0900

    solve english word chain

---
+++ src/test/java/xyz/dddd/algorithm/lesson/EnglishWordChainTest.java
@@ -0,0 +1,29 @@
+package xyz.dddd.algorithm.lesson;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class EnglishWordChainTest {
+
+  private static EnglishWordChain englishWordChain;
+
+  @BeforeAll
+  public static void setUp() {
+    englishWordChain = new EnglishWordChain();
+  }
+
+  @Test
+  public void givenWordsAndPeopleCountWhenFindFirstOutInfoThen() {
+    int peopleCount = 3;
+    String[] words = new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+
+    int[] actual = englishWordChain.findFirstOutInfo(peopleCount, words);
+
+    int[] expected = new int[]{3, 3};
+    assertArrayEquals(expected, actual);
+  }
+
+}
+
