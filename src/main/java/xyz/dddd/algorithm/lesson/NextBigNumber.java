c84c26211e71a170992d99c631b2ba58ea570199
kmira <mira@coyobi.com>
2021-08-09 22:17:14 +0900

    solve next big number

--- src/main/java/xyz/dddd/algorithm/lesson/NextBigNumber.java
+++ src/main/java/xyz/dddd/algorithm/lesson/NextBigNumber.java
@@ -3,31 +3,16 @@ package xyz.dddd.algorithm.lesson;
 public class NextBigNumber {
 
   public int than(final int decimalNumber) {
-    int criteriaOneCount = countOneBinaryString(decimalNumber);
+    int criteriaOneCount = Integer.bitCount(decimalNumber);
 
     int nextNumber = decimalNumber;
     while (true) {
-      if (criteriaOneCount == countOneBinaryString(++nextNumber)) {
+      if (criteriaOneCount == Integer.bitCount(++nextNumber)) {
         break;
       }
     }
 
     return nextNumber;
-  }
-
-  private int countOneBinaryString(final int decimalNumber) {
-    int n = decimalNumber;
-
-    int count = 0;
-    while (n > 0) {
-      if (n % 2 == 1) {
-        count++;
-      }
-
-      n /= 2;
-    }
-
-    return count;
   }
 
 }
