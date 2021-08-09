bebf4c47b4f2a9e4b5e5608be544763b37f76a60
kmira <mira@coyobi.com>
2021-08-09 22:19:48 +0900

    add javadoc for next big number

--- src/main/java/xyz/dddd/algorithm/lesson/NextBigNumber.java
+++ src/main/java/xyz/dddd/algorithm/lesson/NextBigNumber.java
@@ -1,5 +1,13 @@
 package xyz.dddd.algorithm.lesson;
 
+/**
+ * <h1>다음 큰 숫자</h1>
+ * <ul>
+ *   <li>
+ *     <a href="https://programmers.co.kr/learn/courses/30/lessons/12911">Programmers question</a>
+ *   </li>
+ * </ul>
+ */
 public class NextBigNumber {
 
   public int than(final int decimalNumber) {
