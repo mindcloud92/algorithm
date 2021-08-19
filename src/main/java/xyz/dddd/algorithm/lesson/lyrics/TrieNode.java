7dacd88dbc5444ee464c5212abd3632531f79616
kmira <mira@coyobi.com>
2021-08-20 00:10:20 +0900

    Update TrieNode.java
    clean

--- src/main/java/xyz/dddd/algorithm/lesson/lyrics/TrieNode.java
+++ src/main/java/xyz/dddd/algorithm/lesson/lyrics/TrieNode.java
@@ -3,17 +3,7 @@ package xyz.dddd.algorithm.lesson.lyrics;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * <h1>가사 검색 - Trie 자료구조</h1>
- * <ul>
- *   <li>
- *     <a href="https://programmers.co.kr/learn/courses/30/lessons/60060">Programmers question</a>
- *   </li>
- *   <li>
- *     <a href="https://ko.wikipedia.org/wiki/%ED%8A%B8%EB%9D%BC%EC%9D%B4_(%EC%BB%B4%ED%93%A8%ED%8C%85)">Wiki: Trie 자료구조</a>
- *   </li>
- * </ul>
- */
+
 public class TrieNode {
 
   private final Map<Character, TrieNode> children = new HashMap<>();
