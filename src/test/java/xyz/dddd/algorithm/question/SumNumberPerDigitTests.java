0ded34365cb8c439bd82918400c09a5719202dd0
kmira <mira@coyobi.com>
2021-09-10 02:01:10 +0900

    rename file

--- src/test/java/xyz/dddd/algorithm/question/SumNumberPerDigitTests.java
+++ src/test/java/xyz/dddd/algorithm/question/SumNumberPerDigitTests.java
@@ -6,7 +6,7 @@ import xyz.dddd.algorithm.question.programmers.SumNumberPerDigit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AddDigitTests {
+public class SumNumberPerDigitTests {
 
   private static SumNumberPerDigit sumNumberPerDigit;
 
