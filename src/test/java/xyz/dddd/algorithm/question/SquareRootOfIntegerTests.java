3efdefae61fac1d5b23d6baf86fac12af37476f9
kmira <kmira@mz.co.kr>
2021-09-26 17:53:46 +0900

    add question

---
+++ src/test/java/xyz/dddd/algorithm/question/SquareRootOfIntegerTests.java
@@ -0,0 +1,37 @@
+package xyz.dddd.algorithm.question;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SquareRootOfIntegerTests {
+
+  private static SquareRootOfInteger squareRootOfInteger;
+
+  @BeforeAll
+  public static void setUp() {
+    squareRootOfInteger = new SquareRootOfInteger();
+  }
+
+  @Test
+  public void given_SquareNumber_when_PowNextSqrt_then_PowNextSqrt() {
+    int number = 121;
+
+    long actual = squareRootOfInteger.solve(number);
+
+    long expected = 144;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void given_NotSquareNumber_when_PowNextSqrt_then_InvalidNumber() {
+    int number = 3;
+
+    long actual = squareRootOfInteger.solve(number);
+
+    long expected = -1;
+    assertEquals(expected, actual);
+  }
+
+}
