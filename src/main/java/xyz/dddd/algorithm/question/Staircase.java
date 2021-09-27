21aa6254cea57e8f7e363a1022ea38126e7e3082
kmira <kmira@mz.co.kr>
2021-09-27 21:28:24 +0900

    add question

---
+++ src/main/java/xyz/dddd/algorithm/question/Staircase.java
@@ -0,0 +1,25 @@
+package xyz.dddd.algorithm.question;
+
+/**
+ * <h3><a href="https://www.hackerrank.com/challenges/staircase/problem">"Staircase"</a></h1>
+ * <p>· 문제: # 기호와 공백을 사용해서 계단모양을 출력</p>
+ * <p>· 예시: n이 4인 경우</p>
+ * <code>
+ * <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;#</p>
+ * <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;##</p>
+ * <p>&nbsp;&nbsp;&nbsp;&nbsp;###</p>
+ * <p>&nbsp;&nbsp;&nbsp;####</p>
+ * </code>
+ */
+public class Staircase {
+
+  private static final String STAIR_EXPRESSION = "#";
+  private static final String EMPTY_EXPRESSION = " ";
+
+  public void solve(final int n) {
+    for (int i = 1; i < n + 1; i++) {
+      System.out.printf("%s%s\n", EMPTY_EXPRESSION.repeat(n - i), STAIR_EXPRESSION.repeat(i));
+    }
+  }
+
+}
