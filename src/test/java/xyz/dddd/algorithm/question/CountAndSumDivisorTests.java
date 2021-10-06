85581c47fd90383a4bca65eea1c25780dda9442c
kmira <kmira@mz.co.kr>
2021-10-06 21:44:32 +0900

    add question

---
+++ src/test/java/xyz/dddd/algorithm/question/CountAndSumDivisorTests.java
@@ -0,0 +1,28 @@
+package xyz.dddd.algorithm.question;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CountAndSumDivisorTests {
+
+  private static CountAndSumDivisor countAndSumDivisor;
+
+  @BeforeAll
+  public static void setUp() {
+    countAndSumDivisor = new CountAndSumDivisor();
+  }
+
+  @Test
+  public void given_StartAndEnd_when_CountAndSumDivisor_then_Sum() {
+    int start = 13;
+    int end = 17;
+
+    int actual = countAndSumDivisor.solve(start, end);
+
+    int expected = 43;
+    assertEquals(expected, actual);
+  }
+
+}
