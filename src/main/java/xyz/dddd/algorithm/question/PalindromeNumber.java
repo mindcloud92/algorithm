d3b3ce64f0118b0c842fdf3792cfa371a3001694
kmira <mira@coyobi.com>
2021-10-10 12:20:13 +0900

    add question

---
+++ src/main/java/xyz/dddd/algorithm/question/PalindromeNumber.java
@@ -0,0 +1,33 @@
+package xyz.dddd.algorithm.question;
+
+/**
+ * <h3><a href="https://leetcode.com/problems/palindrome-number/">"Palindrome Number"</a></h1>
+ * <p>· 문제: 숫자 x를 앞으로 뒤로 읽어도 똑같이 읽히는 PalindromeNumber인지 여부를 반환</p>
+ * <p>· 조건</p>
+ * <ol>
+ *   <li>-2의 31승 <= x <= (2의 31승 - 1)</li>
+ * </ol>
+ */
+public class PalindromeNumber {
+
+  public boolean solve(final int x) {
+    if (x < 0) {
+      return false;
+    }
+
+    String str = String.valueOf(x);
+    for (int i = 0; i < str.length(); i++) {
+      boolean isMiddleIndex = i == str.length() / 2;
+      if (isMiddleIndex) {
+        break;
+      }
+
+      if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+}
